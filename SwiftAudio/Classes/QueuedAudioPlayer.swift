f6039d9a28fa984e1395bcd534ee68579f52c0f2
Parth Kshatriya <parth0072@live.com>
2021-04-05 20:12:16 +0530

    Update QueuedAudioPlayer.swift

--- SwiftAudio/Classes/QueuedAudioPlayer.swift
+++ SwiftAudio/Classes/QueuedAudioPlayer.swift
@@ -163,7 +163,7 @@ public class QueuedAudioPlayer: AudioPlayer {
      - parameter toIndex: The index to move the item to.
      - throws: `APError.QueueError`
      */
-    func moveItem(fromIndex: Int, toIndex: Int) throws {
+    public func moveItem(fromIndex: Int, toIndex: Int) throws {
         try queueManager.moveItem(fromIndex: fromIndex, toIndex: toIndex)
     }
     
